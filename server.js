d5ea1578da544d5b83042961968e9b25bbac7de4
Stanley <angstanley68@gmail.com>
2019-08-12 17:34:03 +0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const line = require('@line/bot-sdk');
+
+const config = {
+  channelAccessToken: '1608273593',
+  channelSecret: '7a7f9311f615d83bcb67eda3d7538532'
+};
+
+const app = express();
+app.post('/webhook', line.middleware(config), (req, res) => {
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result));
+});
+
+const client = new line.Client(config);
+function handleEvent(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    return Promise.resolve(null);
+  }
+
+  return client.replyMessage(event.replyToken, {
+    type: 'text',
+    text: event.message.text
+  });
+}
+
+app.listen(3000);
